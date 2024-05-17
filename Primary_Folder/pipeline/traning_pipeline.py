1b6c91ec32600ae86e5f292045408eccd0997f52
JRDittenber <jeff.dittenber@hotmail.com>
2024-05-17 14:07:22 -0500

    Updated Gitignore

--- Primary_Folder/pipeline/traning_pipeline.py
+++ Primary_Folder/pipeline/traning_pipeline.py
@@ -2,9 +2,9 @@ import sys
 from Primary_Folder.exceptions import final_except
 from Primary_Folder.logger import logging
 
-from Primary_Folder.components.data_ingestion import DataIngestion
-from Primary_Folder.components.data_validation import DataValidation
-from Primary_Folder.components.data_transformation import DataTransformation
+from Primary_Folder.components.data_ingestion import (DataIngestion)
+from Primary_Folder.components.data_validation import (DataValidation)
+from Primary_Folder.components.data_transformation import (DataTransformation)
 from Primary_Folder.components.model_trainer import ModelTrainer
 from Primary_Folder.components.model_evaluation import ModelEvaluation
 from Primary_Folder.components.model_pusher import ModelPusher
